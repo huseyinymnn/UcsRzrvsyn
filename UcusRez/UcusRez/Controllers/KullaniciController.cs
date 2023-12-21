6be9caf614f3c745e3336cb8d2cbed5dfc2897df
huseyinymnn <huseyin.yaman2@ogr.sakarya.edu.tr>
2023-12-21 22:08:53 +0300

    login ve register sayfasi eklendi

---
+++ UcusRez/UcusRez/Controllers/KullaniciController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UcusRez.Controllers
+{
+	public class KullaniciController : Controller
+	{
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		public IActionResult Register()
+		{
+			return View();
+		}
+	}
+}
